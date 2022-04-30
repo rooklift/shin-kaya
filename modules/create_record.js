e586d2a3fbddbf411516f0f00a11cb7b08ee9663
rooklift <16438795+rooklift@users.noreply.github.com>
2022-04-30 11:01:29 +0100

    Create create_record.js

---
+++ modules/create_record.js
@@ -0,0 +1,62 @@
+"use strict";
+
+const fs = require("fs");
+const path = require("path");
+
+const load_sgf = require("./load_sgf");
+const {replace_all} = require("./utils");
+
+function create_record(root, filepath) {
+
+	let ret = {};
+
+	// Strings...
+
+	for (let key of ["BR", "WR", "EV", "PB", "PW", "DT", "RE"]) {
+		ret[key] = root.get(key);										// get() returns "" if absent, which is what we want.
+	}
+
+	// Ints...
+
+	for (let key of ["HA", "SZ"]) {
+
+		let s = root.get(key);
+		let i = parseInt(s, 10);
+
+		if (!Number.isNaN(i)) {
+			ret[key] = i;
+		} else if (key === "SZ") {
+			ret[key] = 19;
+		} else {
+			ret[key] = 0;
+		}
+	}
+
+	ret.dyer = "";														// FIXME / TODO
+	ret.filename = path.basename(filepath);
+	ret.path = path.dirname(filepath);									// path does not include filename
+
+	// For consistency, lets always use / as a path separator...
+
+	if (global.process && global.process.platform === "win32") {
+		ret.path = replace_all(ret.path, "\\", "/");
+	}
+
+	return ret;
+}
+
+function create_record_from_path(filepath) {							// Can throw
+
+	if (!fs.existsSync(filepath)) {
+		throw new Error("No such file");
+	}
+
+	let buf = fs.readFileSync(filepath);
+	let root = load_sgf(buf);
+
+	return create_record(root, filepath);
+}
+
+
+
+module.exports = {create_record_from_path};
