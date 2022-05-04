9cb65bc75cc7d963b2aff357d4ed44bb34632688
rooklift <16438795+rooklift@users.noreply.github.com>
2022-05-04 18:37:30 +0100

    Update db.js

--- src/modules/db.js
+++ src/modules/db.js
@@ -107,11 +107,15 @@ exports.update = function() {
 
 	work_timeout_id = setTimeout(() => {
 		work_timeout_id = null;
-		really_update();
+		really_update(current_db);
 	}, 5);
 };
 
-function really_update() {
+function really_update(database) {
+
+	if (database !== current_db) {
+		throw new Error("really_update(): database changed unexpectedly");
+	}
 
 	// Make a set of all known files in the database...
 
@@ -179,7 +183,7 @@ function continue_work(database, missing_files, new_files, missing_off, new_off)
 	if (missing_off < missing_files.length || new_off < new_files.length) {
 		work_timeout_id = setTimeout(() => {
 			work_timeout_id = null;
-			continue_work(database, missing_files, new_files, missing_off, new_off)
+			continue_work(current_db, missing_files, new_files, missing_off, new_off)
 		}, 5);
 	} else {
 		document.getElementById("status").innerHTML = `Update completed - deletions: ${missing_files.length}, additions: ${new_files.length}`;
