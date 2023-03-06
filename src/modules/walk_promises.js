3183be10b69da13117a21b6a3f673028d9657843
rooklift <16438795+rooklift@users.noreply.github.com>
2023-03-06 10:24:28 +0000

    skip various db related files

--- src/modules/walk_promises.js
+++ src/modules/walk_promises.js
@@ -4,17 +4,26 @@ const fs = require("fs/promises");
 const slashpath = require("./slashpath");
 const { replace_all } = require("./utils");
 
+const skippable_ends = [".db", ".db-shm", ".db-wal", "journal"];
+
 async function list_all_files(archivepath, relpath) {
 	let ret = [];
 	let read = await fs.readdir(slashpath.join(archivepath, relpath));
 	for (let o of read) {
+		let o_lower = o.toLowerCase();
+		let skip = false;
+		for (let end of skippable_ends) {
+			if (o_lower.endsWith(end)) {
+				skip = true;
+				break;
+			}
+		}
+		if (skip) {
+			continue;
+		}
 		let new_relpath = slashpath.join(relpath, o);
-		if (o.toLowerCase().endsWith(".sgf")) {										// We think this is a file...
+		if (o_lower.endsWith(".sgf")) {												// We think this is a file...
 			ret.push(new_relpath);
-		} else if (o.toLowerCase().endsWith(".db")) {
-			// skip
-		} else if (o.toLowerCase().endsWith("journal")) {
-			// skip
 		} else {																	// We think this is a directory... but maybe not.
 			try {
 				let recurse = await list_all_files(archivepath, new_relpath);
@@ -31,30 +40,30 @@ async function list_all_files(archivepath, relpath) {
 // might be better because it doesn't try to concurrently read multiple folders at once?
 
 function list_all_files_alterative(archivepath, relpath) {
-
 	return fs.readdir(slashpath.join(archivepath, relpath)).then(read => {
-
 		let files = [];
 		let promises = [];
-
 		for (let o of read) {
-
+			let o_lower = o.toLowerCase();
+			let skip = false;
+			for (let end of skippable_ends) {
+				if (o_lower.endsWith(end)) {
+					skip = true;
+					break;
+				}
+			}
+			if (skip) {
+				continue;
+			}
 			let new_relpath = slashpath.join(relpath, o);
-
-			if (o.toLowerCase().endsWith(".sgf")) {									// We think this is a file...
+			if (o_lower.endsWith(".sgf")) {											// We think this is a file...
 				files.push(new_relpath);
-			} else if (o.toLowerCase().endsWith(".db")) {
-				// skip
-			} else if (o.toLowerCase().endsWith("journal")) {
-				// skip
 			} else {																// We think this is a directory... but maybe not.
 				let np = list_all_files(archivepath, new_relpath).catch(err => []);
 				promises.push(np);
 			}
 		}
-
 		// So we have files and promises... return a promise that will construct the final result...
-
 		return Promise.all(promises).then(promised_results => {
 			let result = Array.from(files);
 			for (let arr of promised_results) {
